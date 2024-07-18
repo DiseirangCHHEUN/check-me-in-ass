03b5b548003215c37642b62c5d359e3a10a7474a
DiseirangCHHEUN <cseirang27@gmail.com>
2024-07-18 17:35:30 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/diseirang/check_me_in/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.diseirang.check_me_in
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:check_me_in/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize firebase app
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
